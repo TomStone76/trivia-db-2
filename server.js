873e84defabb9be6522750f7b86865aa7f329656
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-15 18:25:32 -0500

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var db = require("../models");
+var db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
-require("./routes/htmlroutes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() { 

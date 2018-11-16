ffc9a385a931d82abc2e51b187290fc1a9816008
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-16 18:13:23 -0500

    commit

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE qanda_db;
+
+INSERT INTO Questions(question_text, answer_key)
+VALUES ("Paris is the capital of France.", true);
+
+INSERT INTO Questions(question_text, answer_key)
+VALUES ("Cats are canines.", false);
+
+INSERT INTO Questions(question_text, answer_key)
+VALUES ("Dogs are felines.", false);
+
+INSERT INTO Questions(question_text, answer_key)
+VALUES ("China has the largest population of any country.", true);
+
+INSERT INTO Questions(question_text, answer_key)
+VALUES ("JavaScript and Java are the same.", false);

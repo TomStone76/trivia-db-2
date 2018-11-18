4ef51b43cd47dd51afe38dd207cd31e824dbcc61
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-18 14:47:33 -0500

    commit

--- seeds.sql
+++ seeds.sql
@@ -14,3 +14,7 @@ VALUES ("China has the largest population of any country.", true);
 
 INSERT INTO Questions(question_text, answer_key)
 VALUES ("JavaScript and Java are the same.", false);
+
+
+INSERT INTO Users(user_id, username, hatch, correct, incorrect)
+VALUES (1, Person1, abc123, 0, 0);
